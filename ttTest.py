99945fcf0d1e59788da557cbc3794613e235ab6a
Maksitos <Maksitos96@gmail.com>
2022-01-19 14:32:48 +0200

    added draw func

--- ttTest.py
+++ ttTest.py
@@ -1,4 +1,5 @@
-
+import os
+import time
 
 width = 50
 height = 20
@@ -13,7 +14,28 @@ directionBall_x = 1
 directionBall_y = -1
 
 
-def draw(width_p, height_p):
+def draw():
+    height_p = 0
+    while height_p <= height:
+        width_p = 0
+        result = ''
+        while width_p <= width:
+            if ball_y == height_p and width_p == ball_x:
+                result += 'o'
+            elif width_p == 0 and rocket1 <= height_p < rocket1 + rocketLength:
+                result += ']'
+            elif width_p == width and rocket2 <= height_p < rocket2 + rocketLength:
+                result += '['
+            elif height_p == 0:
+                result += 'i'
+            elif height_p == height:
+                result += 'i'
+            else:
+                result += ' '
+            width_p += 1
+        print(result)
+        height_p += 1
+
     pass
 
 
@@ -21,5 +43,7 @@ def moveBall():
     pass
 
 while True:
-    pass
+    draw()
+    time.sleep(0.1)
+    os.system('cls')
 
