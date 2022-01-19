b3c5d6dbdfa459fa36c6385b314f71ba0ba2e595
Maksitos <Maksitos96@gmail.com>
2022-01-19 14:21:18 +0200

    First phase

---
+++ ttTest.py
@@ -0,0 +1,25 @@
+
+
+width = 50
+height = 20
+ball_x = 15
+ball_y = 10
+
+rocket1 = 11
+rocket2 = 8
+rocketLength = 3
+
+directionBall_x = 1
+directionBall_y = -1
+
+
+def draw(width_p, height_p):
+    pass
+
+
+def moveBall():
+    pass
+
+while True:
+    pass
+
